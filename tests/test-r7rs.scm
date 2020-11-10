ef71453411abc945508c403ae2da711f8ca47aa8
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-09 18:09:56 -0800

    Importing srfi-197 instead of including it

--- tests/test-r7rs.scm
+++ tests/test-r7rs.scm
@@ -1,7 +1,7 @@
 (import (scheme base)
         (scheme process-context)
         (scheme write)
-        (srfi 2))
+        (srfi 2)
+        (srfi-197))
 
-(include "./srfi-197.scm")
 (include "./test.scm")
